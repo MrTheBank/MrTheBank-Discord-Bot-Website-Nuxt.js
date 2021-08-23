9ea5de37b2c3b8efae8a91a8f569c608ff6057be
MrTheBank <thebankzana@yahoo.com>
2021-08-23 13:09:55 +0700

    add applications.commands to invite url

--- pages/invite.vue
+++ pages/invite.vue
@@ -2,7 +2,7 @@
 export default {
   name: "invite",
   fetch({redirect}) {
-    redirect('https://discord.com/oauth2/authorize?client_id=530376105182625804&permissions=3164161&scope=bot')
+    redirect('https://discord.com/oauth2/authorize?client_id=530376105182625804&permissions=3164161&scope=bot%20applications.commands')
   }
 }
 </script>
